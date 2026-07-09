0e2490477b8715d8697b663186a94df2a6be4540
Tapasvigowda <tapasvigowda46@gmail.com>
2026-07-09 15:44:23 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,7 +20,7 @@ public class AppTest {
 
         App app = new App();
 
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello this is my World!", app.getMessage());
 
     }
 
